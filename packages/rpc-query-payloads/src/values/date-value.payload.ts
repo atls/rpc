df83bf0a5ec26285d06130e5f4b767a8bbe75ee6
Dmitry <dimafomin02@gmail.com>
2025-03-15 04:27:46 +0300

    feat(rpc-query-payloads): bump @atls/query-types
    fix linter errors and formate files

--- packages/rpc-query-payloads/src/values/date-value.payload.ts
+++ packages/rpc-query-payloads/src/values/date-value.payload.ts
@@ -8,10 +8,12 @@ export class DateValuePayload {
   constructor(value?: Date | Timestamp) {
     if (value instanceof Timestamp) {
       this.value = value.toDate()
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
     } else if ((value as any).toDate) {
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-explicit-any
       this.value = (value as any).toDate()
     } else {
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
       this.value = value!
     }
   }
